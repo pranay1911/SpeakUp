98473af017739a5cd1aaf2522303caf46e1f2497
shubham <pranayjha58411@gmail.com>
2025-06-19 15:41:47 +0530

    1st commit

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGSheP2sjx0EISf9c8I3JQqR1wiqhniDQ",
+  authDomain: "speakupapp-1e117.firebaseapp.com",
+  databaseURL: "https://speakupapp-1e117-default-rtdb.firebaseio.com",
+  projectId: "speakupapp-1e117",
+  storageBucket: "speakupapp-1e117.firebasestorage.app",
+  messagingSenderId: "44307447966",
+  appId: "1:44307447966:web:8daa60ad308de2b06c7b8f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);                                                           
